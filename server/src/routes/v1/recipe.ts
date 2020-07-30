b7b90f18f6b30697d14dd6f4637b489fb7c4ce5b
Samuel Olusola <olusola.samuel.oluwatobi@gmail.com>
2020-07-30 21:15:35 +0100

    authenticating recipe

--- server/src/routes/v1/recipe.ts
+++ server/src/routes/v1/recipe.ts
@@ -1,10 +1,11 @@
 import express, { Router } from "express";
 import recipeController from "../../controllers/v1/recipe.controller";
 import { multerUploads } from "../../middleware/multer";
+import checkAuth from "../../middleware/auth";
 
 const recipeRouter: Router = express.Router();
 
-recipeRouter.post("/create", multerUploads, recipeController.create);
+recipeRouter.post("/create", multerUploads, checkAuth, recipeController.create);
 recipeRouter.get("/all", recipeController.getAll);
 recipeRouter.get("/:id", recipeController.getOne);
 
